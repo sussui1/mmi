4636f498973e597ceb376c201a3d575467b154c2
sussui1 <qiongyusu18@gmail.com>
2026-08-31 06:05:13 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./styles/tokens.css";
 import "./styles/global.css";
 import "./styles/phone.css";
+import "./styles/profile.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
