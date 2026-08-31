c9b7c042be725a2ac0d3bd916f724de9b51bb2da
sussui1 <qiongyusu18@gmail.com>
2026-09-01 00:02:22 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,10 @@ import "./styles/tokens.css";
 import "./styles/global.css";
 import "./styles/phone.css";
 import "./styles/profile.css";
+import "./styles/characters.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
-import "./styles/characters.css";
